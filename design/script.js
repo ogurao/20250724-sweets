2f85fb3ddf6e1ba9b7e6aa7278dcda07aefafae1
ogurao <167517023+ogurao@users.noreply.github.com>
2025-07-24 14:22:53 +0900

    Add files via upload

---
+++ design/script.js
@@ -0,0 +1,24 @@
+// $ (function (){
+//     $ (".list-1",".list-2").slick({
+//         infinite: true,
+//         slidesToShow: 4,
+//         slidesToScroll: 4
+//     });
+// });
+
+$(function (){
+    // .slider-1
+    $(".slider-1").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+
+    // .slider-2
+    $(".slider-2").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
+
+    });
+});
